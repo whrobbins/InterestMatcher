0c33189b901fe313de380b2fe88676603c81d24a
Lucas Message <lucas.message@hotmail.com>
2015-09-22 20:25:05 -0500

    Integrated with firebase (I think), but can't set data to it.

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,8 @@
-
 var Firebase = require("firebase");
-
 var ref = new Firebase("https://blazing-torch-3926.firebaseio.com/");
 
-ref.authWithOAuthPopUp("facebook", function(error, authData)
-	{
-		
-		
-	}
-
-)
+ref.set({
+	name: "Lucas",
+	age: 18,
+	gender: "male",
+})
